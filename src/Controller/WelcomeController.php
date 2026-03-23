9e51daf08d4d3ec8f0275bdd07cd24a97cab3b2f
Steeven HOUNKPE-SAGBO <e.hounkpesagbo@ecoles-epsi.net>
2026-03-23 11:39:05 +0100

    Mise en place du système de signu et login. L'utilisateur peu s'inscrire uniquement si son email existe en base de donnée de l'API principale

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
+final class WelcomeController extends AbstractController
+{
+    #[Route('/welcome', name: 'app_welcome')]
+    public function welcome(Request $request): Response
+    {
+
+    // je récupère la session de l'utilisateur pour afficher son nom dans la page d'accueil
+    $session = $request->getSession();
+    $userName = $session->get('userName');
+ 
+
+        return $this->render('welcome/index.html.twig', [
+            'controller_name' => 'WelcomeController',
+            'userName' => $userName,
+         ]);
+    }
+}
